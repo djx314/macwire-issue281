8e3b02820b3b9469e05c5df4bbcad21de9da6d34
djx314 <djx314@sina.cn>
2023-03-17 09:52:08 +0800

    pythagoras pythagoras python.

--- src/main/scala/macwire/test/app/AppInjection.scala
+++ src/main/scala/macwire/test/app/AppInjection.scala
@@ -2,21 +2,23 @@ package macwire.test.service
 
 import cats.effect._
 import com.softwaremill.macwire.wire
-import macwire.test.resource.{DBResourceA, DBResourceB, Named, PrintStringResource}
+import macwire.test.resource.{DBResource, DBResourceA, DBResourceB, Named, PrintString, PrintStringResource}
 
 class AppInjection {
   lazy val namedInstance: Named = Named("macwire-issue281")
 
-  def serviceCResource[F[_]]: Resource[F, ServiceC] = wire[DBResourceA].resource[F].flatMap { dbA =>
-    wire[DBResourceB].resource[F].flatMap { dbB =>
-      wire[PrintStringResource].resource[F].map { implicit printlnString =>
-        lazy val serviceA: ServiceA = ServiceA(resourceA = dbA) // Injection by name.
-        // Need more friendly.
-        // `test = implicitly` is simply fetching variable `printlnString`.
-        // I think we can do more things about both `implicit printlnString` and `test = implicitly`.
-        lazy val serviceB: ServiceB = ServiceB(resourceB = dbB, test = implicitly) // Injection by name and injection by type.
+  private class ServiceBImpl(test: PrintString) {
+    def build(resourceB: DBResource): ServiceB = ServiceB(resourceB = resourceB, test = test)
+  }
+
+  def serviceCResource[F[_]]: Resource[F, ServiceC] = wire[DBResourceA].resource.flatMap(dbA =>
+    wire[DBResourceB].resource.flatMap(dbB =>
+      wire[PrintStringResource].resource.map { printlnString =>
+        lazy val serviceA: ServiceA         = ServiceA(resourceA = dbA)           // Injection by name.
+        lazy val serviceBImpl: ServiceBImpl = wire[ServiceBImpl]
+        lazy val serviceB: ServiceB         = serviceBImpl.build(resourceB = dbB) // Injection by name and injection by type.
         wire[ServiceC]
       }
-    }
-  }
+    )
+  )
 }
