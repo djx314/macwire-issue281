f2c83cf132fb7a24702b31c914088c47f3505916
djx314 <djx314@sina.cn>
2023-03-18 19:38:37 +0800

    laboratory.laboratory.

--- src/main/scala-3/macwire/test/app/AppInjection.scala
+++ src/main/scala-3/macwire/test/app/AppInjection.scala
@@ -1,18 +1,20 @@
 package macwire.test.service
 
-import cats.effect._
+import cats.Monad
+import cats.effect.Resource
 import macwire.test.resource.{DBResource, DBResourceA, DBResourceB, Named, PrintString, PrintStringResource}
-import cats.Applicative
 
-class AppInjection {
+class AppInjection:
+
+  def applyM[F[_]]: Monad[Resource[F, *]] = Monad[Resource[F, *]]
 
   def serviceCResource[F[_]]: Resource[F, ServiceC] = for
-    given Named       <- Applicative[Resource[F, *]].pure(Named("macwire-issue281"))
+    given Named       <- applyM.pure(Named("macwire-issue281"))
     dbA               <- (new DBResourceA).resource
     dbB               <- (new DBResourceB).resource
     given PrintString <- (new PrintStringResource).resource
-    given ServiceA = ServiceA(using resourceA = dbA)
-    given ServiceB = ServiceB(using resourceB = dbB)
+    given ServiceA    <- applyM.pure(ServiceA(using resourceA = dbA))
+    given ServiceB    <- applyM.pure(ServiceB(using resourceB = dbB))
   yield new ServiceC
 
-}
+end AppInjection
