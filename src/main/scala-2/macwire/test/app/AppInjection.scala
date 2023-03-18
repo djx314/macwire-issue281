d4202c393b2a5920fa6a6f8417bae484f0186f75
djx314 <djx314@sina.cn>
2023-03-18 17:32:17 +0800

    pyton python python python pythagoras pythagoras pythagoras pythagoras pythagoras.

--- src/main/scala-2/macwire/test/app/AppInjection.scala
+++ src/main/scala-2/macwire/test/app/AppInjection.scala
@@ -9,12 +9,9 @@ class AppInjection {
   def serviceCResource[F[_]]: Resource[F, ServiceC] = (new DBResourceA).resource.flatMap { dbA =>
     (new DBResourceB).resource.flatMap { dbB =>
       (new PrintStringResource).resource.map { implicit printlnString =>
-        lazy val serviceA: ServiceA = ServiceA(resourceA = dbA) // Injection by name.
-        // Need more friendly.
-        // `test = implicitly` is simply fetching variable `printlnString`.
-        // I think we can do more things about both `implicit printlnString` and `test = implicitly`.
-        lazy val serviceB: ServiceB = ServiceB(resourceB = dbB, test = implicitly) // Injection by name and injection by type.
-        wire[ServiceC]
+        implicit lazy val serviceA: ServiceA = new ServiceA()(resourceA = dbA)
+        implicit lazy val serviceB: ServiceB = new ServiceB()(resourceB = dbB, test = implicitly)
+        new ServiceC
       }
     }
   }
